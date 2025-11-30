98a09e071e085f2962e50686382d596bc1cccec8
jamal-mousa <jamal.moussa.dev@gmail.com>
2025-11-30 22:09:41 +0200

    implement AddNotesBottomSheet and integrate with NotesView for note creation

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_notes_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -10,7 +11,24 @@ class NotesView extends StatelessWidget {
     (
       floatingActionButton: FloatingActionButton
       (
-        onPressed: (){},
+        onPressed: ()
+        {
+          showModalBottomSheet
+          (
+            shape: RoundedRectangleBorder
+            (
+              borderRadius: BorderRadius.vertical
+              (
+                top: Radius.circular(16)
+              )
+            ),
+            context: context, 
+            builder: (context)
+            {
+              return AddNotesBottomSheet();
+            }
+          );
+        },
         shape: CircleBorder(),
         child: Icon(Icons.add),
       ),
---
+++ lib/widgets/add_notes_bottom_sheet.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
+
+class AddNotesBottomSheet extends StatelessWidget {
+  const AddNotesBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column
+    (
+      children: 
+      [
+        CustomTextField()
+      ],
+    );
+  }
+}
